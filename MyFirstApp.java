6be0d275457f99f1344d7dc85351befb660c51dc
Arthur_Shagaev <artur.doker90@gmail.com>
2023-07-30 21:32:48 +0500

    test

--- MyFirstApp.java
+++ MyFirstApp.java
@@ -1,5 +1,5 @@
 public class MyFirstApp {   
     public static void main(String[] args) {
-        System.out.println("Write once, run anywhere");
+        System.out.println("Write once, run anywhere!");
     }
 }
